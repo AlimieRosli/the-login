84c234753637a83b02c6fba1103ed30a27dcac23
AlimieRosli <muhd_alimie@yahoo.com>
2020-12-08 13:00:08 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'the-login';
+
+  public loginForm;
+  public submitClicked: boolean = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    // validation
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)
+      ])
+    });
+
+    console.log("initial :", this.loginForm);
+  }
+
+  // field access
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+
+  onSubmit(){
+    console.log("clicked :", this.loginForm);
+    this.submitClicked = true;
+
+    // verify validation status
+    if(!this.loginForm.invalid){
+      alert("Submitted");
+    }  
+  }
+
 }
